87ddbc207da69bbc46ae16a120bfc9b14f9b84ed
lyonghwan <longhuan@hatiolab.com>
2021-05-03 12:01:33 +0900

    수정 내용 컴밋

--- socket-client.js
+++ socket-client.js
@@ -4,18 +4,24 @@
 var net = require("net");
 require("console-stamp")(console, { pattern: "dd/mm/yyyy HH:MM:ss.l" });
 
-var client = net.connect({ port: 2222, host: "localhost" }, function () {
+var client = net.connect({ port: 1702, host: "192.168.1.38" }, function () {
+  var connName = "USER";
   console.log(connName + " 연결 됨.");
   console.log(`로컬 : ${this.localAddress} / ${this.localPort}`);
   console.log(`리모트 : ${this.remoteAddress} / ${this.remotePort}`);
   //this라고 써도 됨. this = client
 
-  this.setTimeout(500);
+  // this.setTimeout(500);
   this.setEncoding("utf8");
+
+  this.on("connect", function (data) {
+    console.log(connName + "한테서 데이터가 옴: " + data.toString());
+    client.end();
+  });
 
   this.on("data", function (data) {
     console.log(connName + "한테서 데이터가 옴: " + data.toString());
-    client.end();
+    // client.end();
   });
   this.on("end", function () {
     console.log(connName + "커넥션이 끄너져떠");
@@ -41,6 +47,4 @@ function writeData(socket, data) {
     })(socket, data);
   }
 }
-
-var user = getConnection("user");
 // writeData(user, "user가 접속해따요!!!!!!!!!!!!!!!!!!!!!!");
